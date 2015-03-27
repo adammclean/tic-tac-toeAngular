eb29a41208cccd53267ffdfe4d7ba049eb0e2e97
Adam McLean <adam.mclean1@gmail.com>
2015-03-26 17:43:15 -0700

    add tie game condition and tie div

--- tictacNG.js
+++ tictacNG.js
@@ -20,12 +20,28 @@ $scope.playerMove = function(idx){
   if(($scope.board[idx].val!=='x') && ($scope.board[idx].val!=='o'){ //We check to see if our div is not populated by class 'x' or 'o'
     if($scope.turn%2!==0){ //The first player move is move '1'
       $scope.board[idx].val = 'x';
-      $scope.xMoves.push((idx).toString());
-      console.log($scope.xMoves)
+      $scope.xMoves.push((idx).toString()); //What exactly does pushing our [idx] 'look like'; an 'x'?
+      console.log($scope.xMoves);
     }
+    
+  else{
+    $scope.board[idx].val='x';
+    $scope.oMoves.push((idx).to_String());
+    console.log($scope.oMoves);
+    }
+  
+  if($scope.turnNumber >= 4){
+    console.log("Checking win conditions starting turn 5");
+    console.log($scope.winningCombos.length);
+  }
   })
 }
+  
+  //Can we place our tie game check in the loop above? Does it matter?
 
+  if(turn == 8 && gameOver=false){
+    document.getElementById('outcomeDiv').innerHTML="Tie game!";
+  }
 
 
 
