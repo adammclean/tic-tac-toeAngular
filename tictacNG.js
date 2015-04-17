835682f26f0296ede322fd0fb5ae03431d632d87
Adam McLean <adam.mclean1@gmail.com>
2015-04-17 09:17:44 -0700

    cleanup logic

--- tictacNG.js
+++ tictacNG.js
@@ -3,7 +3,7 @@ var app = angular.module('ticTacApp', []);
 app.controller('ticTacCtrl', function($scope){
 
 
-$scope.turn = 1;
+$scope.turn = 0;
 $scope.xMoves = [];
 $scope.oMoves = [];
 $scope.winner = '';
@@ -12,7 +12,7 @@ $scope.tie = false;
 $scope.winner = '';
 
 
-$scope.winningCombos = [['1','2','3'],['4','5','6'],['7','8','9'],['1','4','7'],['2','5','8'],['3','6','9'],['1','5','9'],['3','5','7']];
+$scope.winningCombos = [['0','1','2'],['3','4','5'],['6','7','8'],['0','3','6'],['1','4','7'],['2','5','8'],['0','4','8'],['2','4','6']];
 
 
 $scope.board = [
@@ -28,7 +28,7 @@ $scope.playerMove = function(idx){
 		return;
 	}
 
-if($scope.turn % 2 !== 0){
+if($scope.turn % 2 == 0){
 	$scope.board[idx].val = 'x';
 	$scope.board[idx].choice = 'assets/mammoth.png';
 	$scope.xMoves.push(idx);
@@ -45,15 +45,14 @@ if($scope.turn % 2 !== 0){
 
 // ***********************Check win conditions**************************
 
-
-	if($scope.turnNumber>=5){
+	if($scope.turn>=4){
 		$scope.winner='';
 	
-		if($scope.turnNumber%2!==0){
+		if($scope.turn%2!==0){
 			console.log('checking win conditions for x');
 
 			for(var i = 0; i<$scope.winningCombos.length;i++){
-				if(($scope.xMoves.indexOf($scope.winningCombos[i][0])!==-1)&&($scope.xMoves.indexOf($scope.winningCombos[i][1])!==-1)&&($scope.xMoves.indexOf($scope.winningCombos[i][2])!==-1)){
+				if(($scope.xMoves.indexOf($scope.winningCombos[i][0])!=-1)&&($scope.xMoves.indexOf($scope.winningCombos[i][1])!=-1)&&($scope.xMoves.indexOf($scope.winningCombos[i][2])!=-1)){
 					console.log('x wins');
 					$scope.gameOver = true;
 					$scope.winner = 'yeti';
@@ -64,7 +63,7 @@ if($scope.turn % 2 !== 0){
 				console.log('checking win conditions for o');
 
 				for(var i = 0; i<$scope.winningCombos.length;i++){
-					if(($scope.oMoves.indexOf($scope.winningCombos[i][0])!==-1)&&($scope.oMoves.indexOf($scope.winningCombos[i][1])!==-1)&&($scope.oMoves.indexOf($scope.winningCombos[i][2])!==-1)){
+					if(($scope.oMoves.indexOf($scope.winningCombos[i][0])!=-1)&&($scope.oMoves.indexOf($scope.winningCombos[i][1])!=-1)&&($scope.oMoves.indexOf($scope.winningCombos[i][2])!=-1)){
 					console.log('o wins');
 					$scope.gameOver = true;
 					$scope.winner = 'unicorn';
@@ -72,8 +71,9 @@ if($scope.turn % 2 !== 0){
 				} 
 			}
 
-			if($scope.turn==8 && !$scope.winner){
+			if(($scope.turn==8) && ($scope.winner == false)){
 					$scope.tie=true;
+					console.log('tie game!')
 				}
 
 			}//end win condition checker
